d642970ad3426fa6b741829172fbdc9f11d7e01b
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2024-09-25 09:46:33 -0700

    fix: change all occurrences of the turtlebot3_datasets to irobo

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-path_to_pkg=`rospack find turtlebot3_datasets`/data
+path_to_pkg=`rospack find irobo`/data
 gdown -O $path_to_pkg/ir_labs.tar.gz https://drive.google.com/uc?id=1-QdzXIoh0ltdDNGuffLqzsPZLnLwSjAj && tar -xvf $path_to_pkg/ir_labs.tar.gz -C $path_to_pkg
